343d326f499ad4fc2a496c3e79518bb96eac9d9f
rochusqwerty <rochus96@gmail.com>
2020-03-25 17:59:48 +0100

    init

---
+++ 04-ASP.NET/Cookbook/Cookbook/Cookbook/Data/RecipeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cookbook.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cookbook.Data
+{
+    public class RecipeContext : DbContext
+    {
+        public RecipeContext(DbContextOptions<RecipeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Recipe> Recipe { get; set; }
+    }
+}
---
+++ 04-ASP.NET/Cookbook/Cookbook/Cookbook/Models/SeedData.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Cookbook.Data;
+using System;
+using System.Linq;
+using Cookbook.Models;
+
+namespace Cookbook.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RecipeContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RecipeContext>>()))
+            {
+                // Look for any movies.
+                if (context.Recipe.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Recipe.AddRange(
+                    new Recipe
+                    {
+                        Title = "When Harry Met Sally",
+                        ReleaseDate = DateTime.Parse("1989-2-12"),
+                        Genre = "Romantic Comedy",
+                        Price = 7.99M
+                    },
+
+                    new Recipe
+                    {
+                        Title = "Ghostbusters ",
+                        ReleaseDate = DateTime.Parse("1984-3-13"),
+                        Genre = "Comedy",
+                        Price = 8.99M
+                    },
+
+                    new Recipe
+                    {
+                        Title = "Ghostbusters 2",
+                        ReleaseDate = DateTime.Parse("1986-2-23"),
+                        Genre = "Comedy",
+                        Price = 9.99M
+                    },
+
+                    new Recipe
+                    {
+                        Title = "Rio Bravo",
+                        ReleaseDate = DateTime.Parse("1959-4-15"),
+                        Genre = "Western",
+                        Price = 3.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
